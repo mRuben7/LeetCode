cd898c087ff6c8be355f1336d32b64d8e11f7c89
rubnMartnez <projects.overstay918@passinbox.com>
2025-10-28 18:54:08 +0100

    add problem 26 structure

---
+++ C++/problem26.cpp
@@ -0,0 +1,23 @@
+// Two Pointers Section
+// 26. Remove Duplicates from Sorted Array
+
+#include <iostream>
+
+#include <vector>
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        return 0;
+    }
+};
+
+
+// === Debug part ==============================================
+
+int main(){
+    std::vector<int> nums{1,1,2};
+    Solution sol{};
+
+    std::cout << sol.removeDuplicates(nums) << std::endl;
+}
