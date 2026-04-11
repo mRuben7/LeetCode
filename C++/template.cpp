c1e051c63d1e1ddda0c188c4a35be14a5136278f
Rubén <98332317+marsi107@users.noreply.github.com>
2026-04-11 13:08:57 +0200

    add template

---
+++ C++/template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <optional>
+
+class Solution {
+public:
+    //function from leetcode goes here
+};
+
+
+// === Debug part ==============================================
+// use clang++ -std=c++20 template.cpp -o template to compile
+
+int main(){
+    //example input
+    std::vector<int> nums{1,1,2};
+
+    Solution sol{};
+
+    //output
+    std::cout << sol.removeDuplicates(nums) << std::endl;
+}
