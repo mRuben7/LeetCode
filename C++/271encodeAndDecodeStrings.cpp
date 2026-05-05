331b1be2e11800c137ed15237b65f1d607777066
Rubén <98332317+marsi107@users.noreply.github.com>
2026-05-05 12:00:31 +0200

    add struct

---
+++ C++/271encodeAndDecodeStrings.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <optional>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    //function from leetcode goes here
+    string encode(vector<string>& strs) {
+
+    }
+
+    vector<string> decode(string s) {
+
+    }
+
+private:
+    const char ETX = 3;
+    const char CODING_MOVEMENT = 10;
+};
+
+
+// === Debug part ==============================================
+// use clang++ -std=c++20 template.cpp -o template to compile
+
+int main(){
+    //example input
+    std::vector<int> nums{1,1,2};
+
+    Solution sol{};
+
+    //output
+    
+}
