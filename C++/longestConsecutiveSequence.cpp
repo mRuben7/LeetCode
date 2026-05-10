185da8e9be5a683f5886f1b93004043a9ea3c534
Rubén <98332317+marsi107@users.noreply.github.com>
2026-05-10 18:28:52 +0200

    add inputs and struct

---
+++ C++/longestConsecutiveSequence.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <optional>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    //function from leetcode goes here
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.empty()) return {};
+        return 0;
+    }
+};
+
+
+// === Debug part ==============================================
+// use clang++ -std=c++20 template.cpp -o template to compile
+
+int main(){
+    //example input
+    std::vector<int> nums{2,20,4,10,3,4,5}; // expected 4
+    std::vector<int> nums2{100,4,200,1,3,2}; // expected 4
+    std::vector<int> nums3{0,3,7,2,5,8,4,6,0,1}; // expected 9
+    std::vector<int> nums4{1,0,1,2}; // expected 3
+    std::vector<int> nums5{0,3,2,5,4,6,1,1}; // expected 7
+
+    Solution sol{};
+
+    //output
+    std::cout << "combos for nums = " << sol.longestConsecutive(nums) << std::endl;
+    std::cout << "-------------------------------------------\n";
+    std::cout << "combos for nums2 = " << sol.longestConsecutive(nums2) << std::endl;
+    std::cout << "-------------------------------------------\n";
+    std::cout << "combos for nums3 = " << sol.longestConsecutive(nums3) << std::endl;
+    std::cout << "-------------------------------------------\n";
+    std::cout << "combos for nums4 = " << sol.longestConsecutive(nums4) << std::endl;
+    std::cout << "-------------------------------------------\n";
+    std::cout << "combos for nums5 = " << sol.longestConsecutive(nums5) << std::endl;
+    std::cout << "-------------------------------------------\n";
+}
