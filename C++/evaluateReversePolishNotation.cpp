3414bb24ae9d1893d6cf75056099342457744fb5
Rubén <98332317+marsi107@users.noreply.github.com>
2026-05-15 19:31:42 +0200

    add solution

--- C++/evaluateReversePolishNotation.cpp
+++ C++/evaluateReversePolishNotation.cpp
@@ -18,26 +18,51 @@ public:
         if (tokens.empty())
             return 0;
         
-        int res = 0;
         std::vector<int> stack{};
         for (const auto& t:tokens){
             if(t.empty())
                 continue;
             char c = t[0];
-            if(std::isdigit(c)){
-                stack.push_back(c);
+            if(std::isdigit(c) || (c == '-' && t.size()>1)){
+                stack.push_back(std::stoi(t)); // to transform from char to int properly
                 continue;
             }
+            int res;
+            int preLast = *(stack.end() - 2);
             switch (c)
             {
             case '+':
-                /* sum last two nums and pop them from the stack */
+                /* sum last two nums and pop them from the stack, then add the res to the stack */
+                res = preLast + stack.back();
+                stack.pop_back();
+                stack.pop_back();
+                stack.push_back(res);
+                break;
+            case '-':
+                res = preLast - stack.back();
+                stack.pop_back();
+                stack.pop_back();
+                stack.push_back(res);
+                break;
+            case '*':
+                res = stack.back() * preLast;
+                stack.pop_back();
+                stack.pop_back();
+                stack.push_back(res);
+                break;
+            case '/':
+                res = preLast / stack.back();
+                stack.pop_back();
+                stack.pop_back();
+                stack.push_back(res);
                 break;
             
             default:
                 break;
             }
         }
+
+        return stack.back();
     }
 
 };
@@ -48,15 +73,21 @@ public:
 
 int main(){
     //example input
-    std::string str{"[(])"};
-    std::string str2{"([{}])"};
+    std::vector<std::string> str{"1","2","+","3","*","4","-"};
+    std::vector<std::string> str2{"2","1","+","3","*"};
+    std::vector<std::string> str3{"4","13","5","/","+"};
+    std::vector<std::string> str4{"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
 
     Solution sol{};
 
     //output
-    std::cout << "is valid = " << sol.isValid(str) << std::endl;
+    std::cout << "str res = " << sol.evalRPN(str) << std::endl;
     std::cout << "-------------------------------------------\n";
-    std::cout << "is valid = " << sol.isValid(str2) << std::endl;
+    std::cout << "str 2 res = " << sol.evalRPN(str2) << std::endl;
+    std::cout << "-------------------------------------------\n";
+    std::cout << "str 3 res = " << sol.evalRPN(str3) << std::endl;
+    std::cout << "-------------------------------------------\n";
+    std::cout << "str 4 res = " << sol.evalRPN(str4) << std::endl;
     std::cout << "-------------------------------------------\n";
 
 }
