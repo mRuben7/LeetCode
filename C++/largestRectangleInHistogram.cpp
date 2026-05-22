253978f3e7953b8469601b27ebe67264c9a4535b
Rubén <98332317+marsi107@users.noreply.github.com>
2026-05-22 16:39:07 +0200

    add rectangle in histogram

---
+++ C++/largestRectangleInHistogram.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <optional>
+#include <cmath>
+
+using namespace std;
+
+struct car{
+    int pos;
+    double movesLeft;
+};
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        
+    }
+};
+
+
+// === Debug part ==============================================
+// use clang++ -std=c++20 template.cpp -o template to compile
+
+int main(){
+    
+
+}
