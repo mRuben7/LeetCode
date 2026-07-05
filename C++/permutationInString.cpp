cc2334f1021eb3b5debb7e10bf7b41f648429ffc
Rubén <98332317+marsi107@users.noreply.github.com>
2026-07-05 20:13:23 +0200

    add solution

--- C++/permutationInString.cpp
+++ C++/permutationInString.cpp
@@ -14,8 +14,52 @@ using namespace std;
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        const std::string* shortStr{};
+        const std::string* longStr{};
+        // determine shortest string and fill map with it
+        if (s1.size() < s2.size()){
+            shortStr = &s1;
+            longStr = &s2;
+            windowSize = s1.size();
+            fillMap(s1);
+        } else {
+            shortStr = &s2;
+            longStr = &s1;
+            windowSize = s2.size();
+            fillMap(s2);
+        }
 
+        int l = 0;
+        std::unordered_map<char, int> currentMap(chars);
+        bool isResetNeeded = false;
+        for (int r=0;r < longStr->size();r++){
+            
+            if (isResetNeeded){
+                isResetNeeded = false;
+                currentMap = chars;
+            }
+            currentMap[longStr->at(r)]--;
+
+            // char not in map, or in map but count ran out
+            while (r < longStr->size() && (currentMap.find(longStr->at(r)) == currentMap.end() || currentMap[longStr->at(r)] < 0)){
+                isResetNeeded = true;
+                r++;
+                l = r;
+            }
+            if (windowSize == (r-l+1)){
+                return true;
+            }
+        }
         return false;
+    }
+private:
+    int windowSize = 0;
+    std::unordered_map<char, int> chars{};
+
+    void fillMap(const std::string& s){
+        for (char c:s){
+            chars[c]++;
+        }
     }
 };
 
