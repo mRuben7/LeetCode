a36c33260ec6f16ec6cac135d357c6804fab75c1
Rubén <98332317+marsi107@users.noreply.github.com>
2026-07-28 12:58:21 +0200

    add partially working solution

---
+++ C++/findMinimumInRotatedSortedArray.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <deque>
+#include <optional>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l<r){
+            int m = l + ((r - l) / 2);
+            if (m != 0 && nums[m] < nums[m - 1]){
+                return nums[m];
+            } 
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return nums[0];
+    }
+};
+
+
+
+
+
+
+// === Debug part ==============================================
+// use clang++ -std=c++20 template.cpp -o template to compile
+
+int main(){
+    Solution sol{};
+
+    std::vector<int> vec {3,4,5,6,1,2};
+
+    std::cout << sol.findMin(vec) << std::endl;
+    std::cout << "-------------------------------------------\n"; // 2
+    
+
+}
